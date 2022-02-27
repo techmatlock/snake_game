bf88ec2fd4e4cb046e148d2e5bf77cf90140e982
Mark Matlock <techmatlock@gmail.com>
2022-02-27 11:59:47 -0600

    Create 3 turtle objects.

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+x_positions = [0, -20, -40]
 
-turtle_object = Turtle()
-turtle_object.shape("square")
-turtle_object.fillcolor("white")
+for turtle_index in range(0, 3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.penup()
+    new_turtle.goto(x_positions[turtle_index], y=0)
+    print(new_turtle.position())
 
 screen.exitonclick()
