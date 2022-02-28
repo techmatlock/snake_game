d584879191d37928b71b7607f06e67afae338381
Mark <techmatlock@gmail.com>
2022-02-27 20:51:47 -0600

    Created new x, y coords. for segments.

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-x_positions = [0, -20, -40]
+screen.tracer(0)
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-for turtle_index in range(0, 3):
-    new_turtle = Turtle(shape="square")
-    new_turtle.color("white")
-    new_turtle.goto(x_positions[turtle_index], y=0)
+segments = []
 
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+        
 screen.exitonclick()
